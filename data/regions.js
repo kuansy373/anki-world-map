b6321e83f18fcc2a050b9e58f5fefc4a73b8f0f6
kuansy373 <kuansy373@gmail.com>
2026-05-30 12:20:51 +0900

    Create subdivisions.js

--- data/regions.js
+++ data/regions.js
@@ -133,28 +133,6 @@ const countryRegions = {
   ],
   Antarctica: [
     'Antarctica',
-  ],
-  Capitals: [
-    'Accra','Ashgabat','Astana','Asmara','Asuncion','Addis Ababa','Athenes','Apia','Abuja','Abu Dhabi','Amsterdam','Alger','Ankara','Antananarivo','Andorra la Vella','Amman',
-    'Islamabad',
-    'Vienna','Vientiane','Victoria','Vilnius',
-    'Windhoek','Wellington',
-    'Ulaanbaatar',
-    'Jerusalem',
-    'Yerevan',
-    'Oslo',
-    'Ottawa',
-    'Cairo','Castries',
-    'Kathmandu','Kabul',
-    'Seoul',
-    'Tokyo',
-  ],
-  'USA States': [
-    'DE','PA','NJ','GA','CT','MA','MD','SC','NH','VA','NY','NC','RI',
-    'VT','KY','TN','OH','LA','IN','MS','IL','AL','ME',
-    'MO','AR','MI','FL','TX','IA','WI','CA','MN','OR',
-    'KS','WV','NV','NE','CO','ND','SD','MT','WA','ID',
-    'WY','UT','OK','NM','AZ','AK','HI','DC'
   ]
 };
 
---
+++ data/subdivisions.js
@@ -0,0 +1,26 @@
+const capitals = [
+  'Accra', 'Ashgabat', 'Astana', 'Asmara', 'Asuncion', 'Addis Ababa', 'Athenes', 'Apia', 'Abuja', 'Abu Dhabi', 'Amsterdam', 'Alger', 'Ankara', 'Antananarivo', 'Andorra la Vella', 'Amman',
+  'Islamabad',
+  'Vienna', 'Vientiane', 'Victoria', 'Vilnius',
+  'Windhoek', 'Wellington',
+  'Ulaanbaatar',
+  'Jerusalem',
+  'Yerevan',
+  'Oslo',
+  'Ottawa',
+  'Cairo', 'Castries',
+  'Kathmandu', 'Kabul',
+  'Seoul',
+  'Tokyo',
+];
+
+const usaStates = [
+  'DE', 'PA', 'NJ', 'GA', 'CT', 'MA', 'MD', 'SC', 'NH', 'VA', 'NY', 'NC', 'RI',
+  'VT', 'KY', 'TN', 'OH', 'LA', 'IN', 'MS', 'IL', 'AL', 'ME',
+  'MO', 'AR', 'MI', 'FL', 'TX', 'IA', 'WI', 'CA', 'MN', 'OR',
+  'KS', 'WV', 'NV', 'NE', 'CO', 'ND', 'SD', 'MT', 'WA', 'ID',
+  'WY', 'UT', 'OK', 'NM', 'AZ', 'AK', 'HI', 'DC'
+];
+
+countryRegions['USA States'] = usaStates;
+countryRegions['Capitals'] = capitals;
